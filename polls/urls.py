7d15a1c8b7013ce9839e2f0dc7026bb5016f2034
Bruno Pinna <gnolus@msn.com>
2018-03-18 00:03:33 +0100

    last commit

--- polls/urls.py
+++ polls/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     # ex: /polls/
-    path('',                           views.index,  name='index'),
+    path('', views.index, name='index'),
     # ex: /polls/id/
-    path('<int:question_id>/',         view.detail,  name='detail'),
+    path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/id/results/
-    path('<int:question_id>/results/', view.results, name='results'),
+    path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/id/vote/
-    path('<int:question_id>/vote/',    view.note,    name='vote'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
