c3e02186517ce9bfda4168d113b03d5fa970346e
Bruno Pinna <gnolus@msn.com>
2018-03-18 19:33:21 +0100

    unit4

--- polls/urls.py
+++ polls/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     # ex: /polls/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/id/
-    path('<int:question_id>/', views.detail, name='detail'),
+    #il nome name è quello usato quando da template richiamiamo url
+    path('<int:question_id>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/id/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/id/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
