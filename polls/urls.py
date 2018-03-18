04f03c61eff895f62edacf1290e988e3ddd380c4
Bruno Pinna <gnolus@msn.com>
2018-03-18 21:06:36 +0100

    unit4

--- polls/urls.py
+++ polls/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/id/
     #il nome name è quello usato quando da template richiamiamo url
-    path('<int:question_id>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /polls/id/results/
-    path('<int:question_id>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /polls/id/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
